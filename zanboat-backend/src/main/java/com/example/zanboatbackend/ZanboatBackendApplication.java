57b3f7de72dd4ca57404b31cb18a980a49374163
miharbi93 <ibrahimraj93@gmail.com>
2024-01-08 09:10:21 +0300

    main

---
+++ zanboat-backend/src/main/java/com/example/zanboatbackend/ZanboatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.zanboatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZanboatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZanboatBackendApplication.class, args);
+	}
+
+}
---
+++ zanboat-backend/src/test/java/com/example/zanboatbackend/ZanboatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zanboatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZanboatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
